718ab55a2049fa26077b1ca8fcce09f80b330fff
swatantra7 <swatantrajha7@gmail.com>
2019-06-23 23:48:15 +0530

    Added Rspec Required Changes

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 # Specify your gem's dependencies in num_to_word_mapper.gemspec
 gemspec
+gem 'shoulda-matchers'
--- spec/num_to_word_mapper/num_to_word_spec.rb
+++ spec/num_to_word_mapper/num_to_word_spec.rb
@@ -1,9 +1,7 @@
 RSpec.describe NumToWordMapper do
+
   it "has a version number" do
     expect(NumToWordMapper::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
