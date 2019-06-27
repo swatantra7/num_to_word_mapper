63458f5d08255e2d283c19f1f0cbd72ac83f68ba
swatantra7 <swatantrajha7@gmail.com>
2019-06-28 01:37:10 +0530

    This commit refs changes in implementation

--- lib/num_to_word_mapper/word_dictionary.rb
+++ lib/num_to_word_mapper/word_dictionary.rb
@@ -6,17 +6,23 @@ module NumToWordMapper
 
     class << self
 
-      # Fetch the word and Store it in length wise hash
+      # Fetch the word and Store it as hash (key as it's equivalent number)
       def words
         {}.tap do |word|
           File.open(FILE_PATH).each_line do |line|
-            line = line.strip
-            if line.length >= 3
-              word[line.length] = [] if word[line.length].nil? || word[line.length].empty?
-              word[line.length].push(line.downcase)
+            line = line.strip.downcase
+            if line.length >= 3 && line.length < 11
+              eq_number = convert_letter_to_number(line)
+              word[eq_number] = [] if word[line.length].nil? || word[line.length].empty?
+              word[eq_number].push(line)
             end
           end
         end
+      end
+
+      # convert the dictionary letter to it's equivalent number
+      def convert_letter_to_number(line)
+        line.split('').map {|char| NumToWordMapper::MappedNumber::WORD_TO_NUM[char]}.join('')
       end
 
     end
